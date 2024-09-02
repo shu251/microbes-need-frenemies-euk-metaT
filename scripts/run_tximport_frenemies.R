14fe78c2b4293f388f8f8a8efd6135fef38c3455
Hu <skhu@connect-172-31-7-245.vpn.tamu.edu>
2024-09-02 09:48:39 -0500

    updated for MCR analysis only

--- scripts/run_tximport_frenemies.R
+++ scripts/run_tximport_frenemies.R
@@ -17,6 +17,8 @@ num_threads <- getDTthreads()
 # Get list of all salmon output files
 files <- Sys.glob("/scratch/group/hu-lab/frenemies/euk-metaT-eukrhythmic-output/salmon/*_quant/quant.sf")
 
+mcr_files <- Sys.glob("/scratch/group/hu-lab/frenemies/euk-metaT-eukrhythmic-output/salmon/MCR_*_quant/quant.sf")
+
 # Import sample list
 sample_merged <- read_delim(file = "input-docs/frenemies-pretximport.txt") 
 # sample_merged
@@ -44,16 +46,11 @@ tx2gene_in <- tax_and_fxn %>%
 
 txi <- tximport::tximport(files, type = "salmon", tx2gene = tx2gene_in, txOut=TRUE, countsFromAbundance = "lengthScaledTPM")
 
-rm(tx2gene_in)
-rm(tax_and_fxn)
-
 print(proc.time()-ptm)
 
 cat("\nPrep txi and merged sample table for DEseq input\n")
-# library(tidyverse)
 
 # Import and align with the txi$counts output
-
 # Convert to data frame.
 sample_merged <- as.data.frame(sample_merged)
 rownames(sample_merged) <- sample_merged$SAMPLE_REP
@@ -71,5 +68,34 @@ rownames(sample_merged)
 save(txi, sample_merged, file = "/scratch/group/hu-lab/frenemies/euk-metaT-eukrhythmic-output/tximport-sept-2024.RData")
 
 cat("\nDONE\n")
+
+
+## MCR at tximport step only:
+txi_mcr <- tximport::tximport(mcr_files, type = "salmon", tx2gene = tx2gene_in, txOut=TRUE, countsFromAbundance = "lengthScaledTPM")
+
+
+print(proc.time()-ptm)
+
+cat("\nPrep txi and merged sample table for DEseq input\n")
+# library(tidyverse)
+
+# Import and align with the txi$counts output
+sample_merged <- read_delim(file = "input-docs/frenemies-pretximport.txt") 
+sample_merged_mcr <- filter(sample_merged, SITE == "MCR")
+
+# Convert to data frame.
+sample_merged_mcr <- as.data.frame(sample_merged_mcr)
+rownames(sample_merged_mcr) <- sample_merged_mcr$SAMPLE_REP
+colnames(txi_mcr$counts) <- rownames(sample_merged_mcr)
+colnames(txi_mcr$abundance) <- rownames(sample_merged_mcr)
+colnames(txi_mcr$length) <- rownames(sample_merged_mcr)
+
+cat("\nCheck MCR colnames for txi are correct\n")
+colnames(txi_mcr$counts)
+
+cat("\nAnd they should match these rownames:\n")
+rownames(sample_merged_mcr)
+
+save(txi_mcr, sample_merged_mcr, file = "/scratch/group/hu-lab/frenemies/euk-metaT-eukrhythmic-output/tximport-MCR-sept-2024.RData")
 
 # Saves txi object, as this step requires a lot of memory to run.
